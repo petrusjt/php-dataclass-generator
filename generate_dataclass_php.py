e01a6a65bd628c50b6a51b46f6825a49cd6e28ba
petrusjt <jozsefpetrus99@gmail.com>
2020-10-15 09:11:10 +0200

    Made code much readable

--- generate_dataclass_php.py
+++ generate_dataclass_php.py
@@ -3,12 +3,12 @@ import codecs
 
 var_names = sys.argv[2:]
 
-with codecs.open(sys.argv[1], "w", "utf-8") as f:
+with codecs.open(sys.argv[1] + ".php", "w", "utf-8") as f:
     f.write(
 f"""<?php
 
-class {sys.argv[1][:-4]}
-\u007B
+class {sys.argv[1]}
+{{
 """)
     for var_name in var_names:
         f.write(f"\tprivate ${var_name};\n")
@@ -18,12 +18,12 @@ class {sys.argv[1][:-4]}
     f.write("\t//Konstruktor null alapértelmezett paraméterekkel\n\n")
     # Constructor generation with nulled out default values for all the fields
     f.write("\tpublic function __construct(")
+    # Listing all the parameters for the constructor
     for var_name in var_names:
         if var_name != var_names[-1]:
             f.write(f"${var_name}=null, ")
         else:
-            f.write(f"${var_name}=null")
-    f.write(")\n")
+            f.write(f"${var_name}=null)\n")
     f.write("\t{\n")
     for var_name in var_names:
         f.write(f"\t\t$this->{var_name} = ${var_name};\n")
@@ -31,27 +31,30 @@ class {sys.argv[1][:-4]}
 
     f.write("\n")
 
-    f.write("\t//Getterek/Setterek\n\n")
+    f.write("\t//Getterek/Setterek\n")
     # Getters/setters
     for var_name in var_names:
-        f.write(f"\tpublic function get{var_name[0].upper() + var_name[1:]}()\n")
-        f.write("\t{\n")
-        f.write(f"\t\treturn $this->{var_name};\n")
-        f.write("\t}\n")
-        f.write(f"\tpublic function set{var_name[0].upper() + var_name[1:]}(${var_name})\n")
-        f.write("\t{\n")
-        f.write(f"\t\t$this->{var_name} = ${var_name};\n")
-        f.write("\t}\n")
+        f.write(f"""
+    public function get{var_name[0].upper() + var_name[1:]}()
+    {{
+        return $this->{var_name};
+    }}
+    public function set{var_name[0].upper() + var_name[1:]}(${var_name})
+    {{
+        $this->{var_name} = ${var_name};
+    }}
+        """)
     f.write("\n")
     
-    f.write("\t//Builder pattern-szerűség\n\n")
+    f.write("\t//Builder pattern-szerűség\n")
     # Builder pattern
     for var_name in var_names:
-        f.write(f"\tpublic function {var_name[0].upper() + var_name[1:]}()\n")
-        f.write("\t{\n")
-        f.write(f"\t\t$this->{var_name} = ${var_name};\n")
-        f.write("\t\treturn $this;\n")
-        f.write("\t}\n")
+        f.write(f"""
+    public function {var_name[0].upper() + var_name[1:]}(${var_name})
+    {{
+        $this->{var_name} = ${var_name};
+    }}
+""")
 
     f.write("}\n")
     f.write("?>\n")
